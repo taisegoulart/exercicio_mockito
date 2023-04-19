80dc83b8bef6f8b4ae34e9068e64c83fd818c6e6
taisegoulart <taise.m.goulart@gmail.com>
2023-04-19 00:03:09 -0300

    Criando um mock e injetando

---
+++ mockito-vvs/src/test/java/dev/ifrs/VolumeTest.java
@@ -0,0 +1,25 @@
+package dev.ifrs;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class VolumeTest {
+    
+    // Criando um mock da classe AudioManager
+@Mock
+AudioManager audio;
+    // Injetando um mock da classe VolumeUtil
+    @InjectMocks
+    VolumeUtil volume;
+
+@Test
+public void testAudioManagerSetVolume() {
+    // TODO 3 - realize uma chamada para o método maximizeVolume do objeto VolumeUtil
+
+    // TODO 4 - Verifique se o método setVolume foi chamado uma única vez --> usar assert
+}
+}
